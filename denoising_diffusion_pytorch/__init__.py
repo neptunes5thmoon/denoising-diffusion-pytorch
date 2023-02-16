03764feac0f59fe1a94772eee5cda1f1c9254849
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2023-02-16 12:40:43 -0500

    refactor to separate out architecture

--- denoising_diffusion_pytorch/__init__.py
+++ denoising_diffusion_pytorch/__init__.py
@@ -1,14 +1,24 @@
-from denoising_diffusion_pytorch.continuous_time_gaussian_diffusion import \
-    ContinuousTimeGaussianDiffusion
+from denoising_diffusion_pytorch.architecture import Unet
+from denoising_diffusion_pytorch.continuous_time_gaussian_diffusion import (
+    ContinuousTimeGaussianDiffusion,
+)
 from denoising_diffusion_pytorch.denoising_diffusion_pytorch import (
-    DataLoader, Dataset, GaussianDiffusion, Trainer, Unet)
+    DataLoader,
+    Dataset,
+    GaussianDiffusion,
+    Trainer,
+)
 from denoising_diffusion_pytorch.denoising_diffusion_pytorch_1d import (
-    GaussianDiffusion1D, Unet1D)
-from denoising_diffusion_pytorch.elucidated_diffusion import \
-    ElucidatedDiffusion
-from denoising_diffusion_pytorch.learned_gaussian_diffusion import \
-    LearnedGaussianDiffusion
-from denoising_diffusion_pytorch.v_param_continuous_time_gaussian_diffusion import \
-    VParamContinuousTimeGaussianDiffusion
-from denoising_diffusion_pytorch.weighted_objective_gaussian_diffusion import \
-    WeightedObjectiveGaussianDiffusion
+    GaussianDiffusion1D,
+    Unet1D,
+)
+from denoising_diffusion_pytorch.elucidated_diffusion import ElucidatedDiffusion
+from denoising_diffusion_pytorch.learned_gaussian_diffusion import (
+    LearnedGaussianDiffusion,
+)
+from denoising_diffusion_pytorch.v_param_continuous_time_gaussian_diffusion import (
+    VParamContinuousTimeGaussianDiffusion,
+)
+from denoising_diffusion_pytorch.weighted_objective_gaussian_diffusion import (
+    WeightedObjectiveGaussianDiffusion,
+)
---
+++ denoising_diffusion_pytorch/convenience.py
@@ -0,0 +1,18 @@
+def exists(x):
+    return x is not None
+
+
+def default(val, d):
+    if exists(val):
+        return val
+    return d() if callable(d) else d
+
+
+def identity(t, *args, **kwargs):
+    return t
+
+
+def cycle(dl):
+    while True:
+        for data in dl:
+            yield data
