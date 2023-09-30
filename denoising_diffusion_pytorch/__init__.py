e77f13462e0b4ab8bb761ffd07fe29c21ac2a3f2
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2023-09-29 22:44:42 -0400

    feat(core): :sparkles: make dataset customizable and add zarr dataset
    Trainer now expects a dataset instead of a folder as input
    
    feat(core): :sparkles: make dataset customizable and add zarr dataset
    
    Trainer now expects a dataset instead of a folder as input
    
    style(style): :art: Run black to format the code.
    
    fix(core): :sparkles: fix init for new custom datasets
    
    fix(core): :pencil2: fix typo in import in init

--- denoising_diffusion_pytorch/__init__.py
+++ denoising_diffusion_pytorch/__init__.py
@@ -2,7 +2,7 @@ from denoising_diffusion_pytorch.denoising_diffusion_pytorch import (
     GaussianDiffusion,
     Trainer,
 )
-from denoising_diffusion_pytorch.datasets import SimpleDatset, ZarrDataset
+from denoising_diffusion_pytorch.datasets import SimpleDataset, ZarrDataset
 from denoising_diffusion_pytorch.architecture import Unet
 from denoising_diffusion_pytorch.learned_gaussian_diffusion import (
     LearnedGaussianDiffusion,
