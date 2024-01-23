0429c38ac564749e17fc84b9d6c8ab37d301b66b
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2024-01-23 12:44:39 -0500

    feat: :sparkles: separate out saver, rename to exporter and support png exporting
    remove support for timeseries, do not include gridding in name

--- src/denoising_diffusion_pytorch/__init__.py
+++ src/denoising_diffusion_pytorch/__init__.py
@@ -1,35 +1,34 @@
+from denoising_diffusion_pytorch.architecture import Unet
+from denoising_diffusion_pytorch.continuous_time_gaussian_diffusion import (
+    ContinuousTimeGaussianDiffusion,
+)
+from denoising_diffusion_pytorch.datasets import (
+    CellMapDataset3Das2D,
+    CellMapDatasets3Das2D,
+    SimpleDataset,
+    ZarrDataset,
+)
 from denoising_diffusion_pytorch.denoising_diffusion import (
     GaussianDiffusion,
     Trainer,
 )
-from denoising_diffusion_pytorch.datasets import (
-    SimpleDataset,
-    ZarrDataset,
-    CellMapDatasets3Das2D,
-    CellMapDataset3Das2D,
+from denoising_diffusion_pytorch.denoising_diffusion_1d import (
+    Dataset1D,
+    GaussianDiffusion1D,
+    Trainer1D,
+    Unet1D,
 )
+from denoising_diffusion_pytorch.elucidated_diffusion import ElucidatedDiffusion
 from denoising_diffusion_pytorch.exporters import (
-    InferenceSaver,
-    PreProcessOptions,
+    PostProcessOptions,
+    SampleExporter,
 )
-from denoising_diffusion_pytorch.architecture import Unet
 from denoising_diffusion_pytorch.learned_gaussian_diffusion import (
     LearnedGaussianDiffusion,
 )
-from denoising_diffusion_pytorch.continuous_time_gaussian_diffusion import (
-    ContinuousTimeGaussianDiffusion,
-)
-from denoising_diffusion_pytorch.weighted_objective_gaussian_diffusion import (
-    WeightedObjectiveGaussianDiffusion,
-)
-from denoising_diffusion_pytorch.elucidated_diffusion import ElucidatedDiffusion
 from denoising_diffusion_pytorch.v_param_continuous_time_gaussian_diffusion import (
     VParamContinuousTimeGaussianDiffusion,
 )
-
-from denoising_diffusion_pytorch.denoising_diffusion_1d import (
-    GaussianDiffusion1D,
-    Unet1D,
-    Trainer1D,
-    Dataset1D,
+from denoising_diffusion_pytorch.weighted_objective_gaussian_diffusion import (
+    WeightedObjectiveGaussianDiffusion,
 )
--- src/denoising_diffusion_pytorch/exporters.py
+++ src/denoising_diffusion_pytorch/exporters.py
@@ -1,14 +1,17 @@
 import logging
-from typing import Sequence
-import torch
-from torchvision import utils
-from enum import Enum
 import math
-from functools import partial
 import os
+from enum import Enum
+from functools import partial
+from typing import Sequence
+
+import torch
 import zarr
+from PIL import Image
+from torchvision import utils
 
 logger = logging.getLogger(__name__)
+
 
 def get_next_sample(existing: Sequence[str], digits=None):
     if len(existing) < 1:
@@ -26,30 +29,30 @@ def get_next_sample(existing: Sequence[str], digits=None):
     next_sample_str = "{num:0{digits}d}".format(num=next_sample, digits=digits)
     return next_sample_str
 
-def adjust_range(img: torch.Tensor, values=(0, 255)) -> torch.Tensor:
+
+def adjust_range(img: torch.Tensor, *args, values=(0, 255)) -> torch.Tensor:
     return img.mul(values[1]).clamp_(values[0], values[1])
 
 
-def to_numpy(img: torch.Tensor) -> np.array:
+def to_numpy(img: torch.Tensor, *args) -> np.array:
     return img.numpy()
 
 
-def to_cpu(img: torch.Tensor) -> torch.Tensor:
+def to_cpu(img: torch.Tensor, *args) -> torch.Tensor:
     return img.to("cpu")
 
 
-def to_dtype(img: torch.Tensor, dtype=torch.uint8) -> torch.Tensor:
+def to_dtype(img: torch.Tensor, *args, dtype=torch.uint8) -> torch.Tensor:
     return img.to(dtype)
 
-
-def griddify(img: torch.Tensor) -> torch.Tensor:
+def griddify(img: torch.Tensor, *args) -> torch.Tensor:
     num_samples = img.shape[0]
     samples_per_row = int(math.sqrt(num_samples))
     img = utils.make_grid(img, samples_per_row)
     return img
 
 
-class PreProcessOptions(Enum):
+class PostProcessOptions(Enum):
     TO_UINT8 = partial(to_dtype, dtype=torch.uint8)
     ADJUST_RANGE_0_255 = partial(adjust_range, values=(0, 255))
     TO_NUMPY = partial(to_numpy)
@@ -60,32 +63,53 @@ class PreProcessOptions(Enum):
         return self.value(*args)
 
 
-
-class InferenceSaver: # todo: rename
-    def __init__(self, channel_assignment, sample_digits=5):
+class SampleExporter(object):
+    def __init__(self, channel_assignment, sample_digits=5, file_format=".zarr", sample_batch_size=1):
         self.sample_digits = sample_digits
         self.channel_assignment = channel_assignment
+        self.file_format = file_format
+        self.sample_batch_size = sample_batch_size
 
-    def save_sample(self, path, samples, timesteps: Sequence[int | str]):
-        num_samples = samples.shape[0]
-        samples_per_row = int(math.sqrt(num_samples))
-        if samples.ndim == 5 and len(timesteps) != samples.shape[1]:
-            raise ValueError(
-                f"Number of timesteps ({len(timesteps)}) doesn't match length of time dimension ({samples.shape[1]})"
-            )
-        zarr_path = os.path.join(path, "samples", f"grid_{num_samples}.zarr")
-        zarr_grp = zarr.group(store=zarr.DirectoryStore(zarr_path))
+    def _make_dir_zarr(self, path):
+        zarr_grp = zarr.group(store=zarr.DirectoryStore(path))
         next_sample = get_next_sample(zarr_grp.keys(), digits=self.sample_digits)
         sample_grp = zarr_grp.require_group(next_sample)
-        for k, t in enumerate(timesteps):
-            time_grp = sample_grp.require_group(t)
-            if samples.ndim == 5:
-                time_sample = samples[:, k, ...]
+        return sample_grp
+
+    def _save_img_zarr(self, grp, name, data):
+        grp.create_dataset(name, data=data)
+
+    def _make_dir_png(self, path):
+        os.makedirs(path, exist_ok=True)
+        next_sample = get_next_sample(os.listdir(path), digits=self.sample_digits)
+        sample_dir = os.path.join(path, next_sample)
+        os.makedirs(sample_dir)
+        return sample_dir
+
+    def _save_img_png(self, path, name, data):
+        img = Image.fromarray(data.permute(1, 2, 0))
+        fp = os.path.join(path, f"{name}.png")
+        img.save(fp, format="PNG")
+
+    def save_sample(self, path, samples):
+        for batch_start in range(0, samples.shape[0] - self.sample_batch_size + 1, self.sample_batch_size):
+            sample = samples[batch_start : batch_start + self.sample_batch_size]
+            if self.file_format == ".zarr":
+                sample_path = self._make_dir_zarr(path)
+            elif self.file_format == ".png":
+                sample_path = self._make_dir_png(path)
             else:
-                time_sample = samples
+                msg = f"Unknown file format ({self.file_format}) requested."
+                raise ValueError(msg)
             for img_name, (channel_slice, preprocessfuncs) in self.channel_assignment.items():
-                img_data = time_sample[:, slice(*channel_slice), ...]
+                img_data = sample[:, slice(*channel_slice), ...]
                 for func_option in preprocessfuncs:
                     if func_option is not None:
-                        img_data = func_option(img_data)
-                time_grp.create_dataset(img_name, data=img_data)
+                        img_data = func_option(img_data, img_name)
+                if self.file_format == ".zarr":
+                    self._save_img_zarr(sample_path, img_name, img_data)
+                elif self.file_format == ".png":
+                    self._save_img_png(sample_path, img_name, img_data)
+                else:
+                    msg = f"Unknown file format ({self.file_format}) requested."
+                    raise ValueError(msg)
